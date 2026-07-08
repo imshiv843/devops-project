512a52d0c312002de0bc1d78c792c1f46d4f14d4
Shiv Vendra Singh <shivambharat2004@gmail.com>
2026-07-08 14:09:20 +0000

    Initial commit:basic Flask app with health check

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return jsonify({"message":"DevOps project is live!"})
+@app.route('/health')
+def health():
+    return jsonify({"status": "healthy"}),200
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
